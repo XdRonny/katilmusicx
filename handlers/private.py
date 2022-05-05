e5816b1ceb872a260a0d3a8905a87b9ef9e94616
Guddu7866 <93326889+Guddu7866@users.noreply.github.com>
2022-05-05 15:03:55 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -14,20 +14,20 @@ from config import (
 async def start_(client: Client, message: Message):
      await message.reply_photo(
         photo=f"https://telegra.ph/file/13fba0e9d76c406ae9ce2.jpg",
-        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [༒︎★•亗『𝐊𝐀𝐓𝐈𝐋』亗•★ ](https://t.me/tera_baap_katil)
+        caption=f"""**𝐓𝐡𝐢𝐬 𝐈𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝 𝐁𝐲 = [༒︎★•亗『Broken-mr-z』亗•★ ](https://t.me/iam_your_heart4)
         
         
-𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [༒︎★•亗『𝐊𝐀𝐓𝐈𝐋』亗•★ ](https://t.me/tera_baap_katil)**""",
+𝐈𝐟 𝐘𝐨𝐮 𝐇𝐚𝐯𝐞 𝐀𝐧𝐲 𝐐𝐮𝐞𝐬𝐭𝐢𝐨𝐧𝐬 𝐀𝐧𝐝 𝐇𝐞𝐥𝐩 𝐓𝐡𝐞𝐧 𝐃𝐦 𝐌𝐲 𝐁𝐨𝐬𝐬 = [༒︎★•亗『Broken-mr-z』亗•★ ](https://t.me/iam_your_heart4)**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("✚ Add me to your Group", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
                 ],[
                     InlineKeyboardButton("👤 𝐂𝐫𝐞𝐚𝐭𝐨𝐫", url=f"https://t.me/{OWNER_USERNAME}"),
-                    InlineKeyboardButton("⚙️ 𝐒𝐨𝐮𝐫𝐜𝐞 ", url=f"https://github.com/bhumiharsaurabh/katilmusicx")
+                    InlineKeyboardButton("", url=f"https://github.com/bhumiharsaurabh/katilmusicx")
                 ],[
-                    InlineKeyboardButton("📨 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 ", url=f"https://t.me/full_masti_clubs"),
-                    InlineKeyboardButton("📨 𝐔𝐩𝐝𝐚𝐭𝐞", url=f"https://t.me/heartbrokenperson1")
+                    InlineKeyboardButton("", url=f"https://t.me/full_masti_clubs"),
+                    InlineKeyboardButton("", url=f"https://t.me/heartbrokenperson1")
                 ],[
                     InlineKeyboardButton("🔍 How To Use? Commands", callback_data="cb_cmd")
                 ],
@@ -36,7 +36,7 @@ async def start_(client: Client, message: Message):
     )
 
 
-@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
+@Client.on_message(command(["pingg", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
 async def ping_pong(c: Client, message: Message):
     start = time()
     m_reply = await message.reply_text("pinging...")
@@ -44,14 +44,14 @@ async def ping_pong(c: Client, message: Message):
     await m_reply.edit_text("**× I am Alive ×**\n\n@heartbrokenperson1 📡")
 
 
-@Client.on_message(command(["repo"]) & ~filters.edited)
+@Client.on_message(command(["repoo"]) & ~filters.edited)
 async def help(client: Client, message: Message):
     await message.reply_text("`Click on the Button given below to Get the Bot Source Code.`",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "⚙️ 𝐒𝐨𝐮𝐫𝐜𝐞 ", url=f"https://github.com/bhumiharsaurabh/katilmusicx")
+                        "", url=f"https://github.com/bhumiharsaurabh/katilmusicx")
                 ]
             ]
         ),
